59536362f7045e0c866ea1c8884d70e4cafec327
Yuki MIZUNO <mizuyuu0904@gmail.com>
2024-02-11 21:48:28 +0900

    Add bookmark feed implementation

--- src/feed.ts
+++ src/feed.ts
@@ -1,12 +1,51 @@
+import { drizzle } from 'drizzle-orm/d1';
 import type { Context } from 'hono';
+import { env } from 'hono/adapter';
+import { bookmarks } from './schema';
+import { and, desc, eq, lte, sql } from 'drizzle-orm';
+import { decode } from 'hono/jwt';
 
-export function getFeedSkeleton(c: Context) {
-  const feed = [
-    {
-      post: 'at://did:plc:tq2kkfw7mkrt7rnmih4iq5dn/app.bsky.feed.post/3kl2yskq2p22v',
-    },
-  ];
-  const cid = 'bafyreiby2en7vjm2gp77peuosudy72bbhooycv7y45phtr4xdwltjlogdu';
-  const cursor = `${new Date(0).getTime()}::${cid}`;
+export async function getFeedSkeleton(c: Context) {
+  const jwt = c.req
+    .header('Authorization')
+    ?.match(/^Bearer ([\w-]+\.[\w-]+\.[\w-]+)/)?.[1];
+  if (!jwt) {
+    return c.json({ feed: [] });
+  }
+
+  // TODO: verify jwt
+  const {
+    payload: { iss, exp },
+  } = decode(jwt);
+  if (exp * 1000 < Date.now()) {
+    // token expired
+    return c.json({ feed: [] });
+  }
+
+  const { DB } = env<{ DB: D1Database }>(c);
+  const db = drizzle(DB);
+
+  const limit = parseInt(c.req.query('limit') ?? '50', 10);
+  const time = c.req.query('cursor')?.match(/^(\d+)::([\w]+)$/)?.[1];
+  const filter = time
+    ? lte(sql`unixepoch(${bookmarks.updatedAt})`, +time)
+    : undefined;
+
+  const result = await db
+    .select({
+      uri: bookmarks.uri,
+      cid: bookmarks.cid,
+      updatedAt: sql`unixepoch(${bookmarks.updatedAt})`,
+    })
+    .from(bookmarks)
+    .orderBy(desc(bookmarks.updatedAt))
+    .limit(limit)
+    .where(and(eq(bookmarks.sub, iss), eq(bookmarks.isDeleted, 0), filter));
+
+  const feed = result.map((item) => ({ post: item.uri }));
+  const lastPost = result[result.length - 1];
+  const cursor = lastPost
+    ? `${lastPost.updatedAt}::${lastPost.cid}`
+    : undefined;
   return c.json({ cursor, feed });
 }
