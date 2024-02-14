40494e2c894e5a21bdbbfe3c0846dc1aa8585205
Yuki MIZUNO <mizuyuu0904@gmail.com>
2024-02-15 04:59:48 +0900

    Add caching for well-knonw/did.json response

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ import { signUpPage } from './page';
 const app = new Hono();
 app.notFound((c) => c.json({ message: 'Not Found', error: 'not found' }, 404));
 app.get('/', (c) => c.html(signUpPage));
-app.get('/.well-known/did.json', wellKnown);
+app.get('/.well-known/did.json', ...wellKnown);
 app.get('/xrpc/app.bsky.feed.getFeedSkeleton', ...getFeedSkeletonHandlers);
 app.post('/api/register', ...registerAccount);
 app.post('/api/bookmark', ...postBookmarkHandlers);
--- src/well-known.ts
+++ src/well-known.ts
@@ -1,27 +1,33 @@
-import type { Context } from 'hono';
 import { env } from 'hono/adapter';
 import { HTTPException } from 'hono/http-exception';
+import { createFactory } from 'hono/factory';
+import { cache } from 'hono/cache';
 
-export function wellKnown(c: Context) {
-  const { FEED_HOST } = env<{ FEED_HOST: string }>(c);
-  if (new URL(c.req.url).host !== FEED_HOST) {
-    throw new HTTPException(404, {
-      res: c.json({ message: 'Not Found', error: 'not found' }, 404),
+const factory = createFactory();
+
+export const wellKnown = factory.createHandlers(
+  cache({ cacheName: 'well-known' }),
+  (c) => {
+    const { FEED_HOST } = env<{ FEED_HOST: string }>(c);
+    if (new URL(c.req.url).host !== FEED_HOST) {
+      throw new HTTPException(404, {
+        res: c.json({ message: 'Not Found', error: 'not found' }, 404),
+      });
+    }
+
+    return c.json({
+      '@context': ['https://www.w3.org/ns/did/v1'],
+      id: `did:web:${FEED_HOST}`,
+      alsoKnownAs: [],
+      authentication: null,
+      verificationMethod: [],
+      service: [
+        {
+          id: '#bsky_fg',
+          type: 'BskyFeedGenerator',
+          serviceEndpoint: `https://${FEED_HOST}`,
+        },
+      ],
     });
-  }
-
-  return c.json({
-    '@context': ['https://www.w3.org/ns/did/v1'],
-    id: `did:web:${FEED_HOST}`,
-    alsoKnownAs: [],
-    authentication: null,
-    verificationMethod: [],
-    service: [
-      {
-        id: '#bsky_fg',
-        type: 'BskyFeedGenerator',
-        serviceEndpoint: `https://${FEED_HOST}`,
-      },
-    ],
-  });
-}
+  },
+);
