3f7ceeb43dcbf8ae3c77021fa196bd2ee0c840bb
Yuki MIZUNO <mizuyuu0904@gmail.com>
2024-02-24 20:06:25 +0900

    Add request url query logger

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,19 @@
 import { Hono } from 'hono';
+import { createMiddleware } from 'hono/factory';
 import { deleteBookmarkHandlers, postBookmarkHandlers } from './api/bookmark';
 import { registerAccount } from './api/register';
 import { signUpPage } from './page';
 import { wellKnown } from './well-known';
 import { getFeedSkeletonHandlers } from './xrpc/feed';
 
+const queryLogger = createMiddleware(async (c, next) => {
+  const query = c.req.query();
+  console.log('Request query:', query);
+  return next();
+});
+
 const app = new Hono();
+app.use(queryLogger);
 app.notFound((c) => c.json({ message: 'Not Found', error: 'not found' }, 404));
 app.get('/', (c) => c.html(signUpPage));
 app.get('/.well-known/did.json', ...wellKnown);
