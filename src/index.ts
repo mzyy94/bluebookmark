05db408da617489a47aac057a09bc9006437d151
Yuki MIZUNO <mizuyuu0904@gmail.com>
2024-02-25 17:39:03 +0900

    Change static files serving method

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
+import manifest from '__STATIC_CONTENT_MANIFEST';
 import { Hono } from 'hono';
+import { serveStatic } from 'hono/cloudflare-workers';
 import { deleteBookmarkHandlers, postBookmarkHandlers } from './api/bookmark';
 import { registerAccount } from './api/register';
 import { errorLogger, queryLogger } from './logger';
-import { signUpPage } from './page';
 import { wellKnown } from './well-known';
 import { getFeedSkeletonHandlers } from './xrpc/feed';
 
@@ -10,7 +11,7 @@ const app = new Hono();
 app.use(queryLogger);
 app.use(errorLogger);
 app.notFound((c) => c.json({ message: 'Not Found', error: 'not found' }, 404));
-app.get('/', (c) => c.html(signUpPage));
+app.get('/*', serveStatic({ root: './', manifest }));
 app.get('/.well-known/did.json', ...wellKnown);
 app.get('/xrpc/app.bsky.feed.getFeedSkeleton', ...getFeedSkeletonHandlers);
 app.post('/api/register', ...registerAccount);
