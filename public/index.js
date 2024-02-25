f5bb1212f6aeedfaacdd3f4188dd5386d7c7df16
Yuki MIZUNO <mizuyuu0904@gmail.com>
2024-02-25 21:52:30 +0900

    Enable client-side JS type check

--- public/index.js
+++ public/index.js
@@ -1,5 +1,16 @@
+// @ts-check
+/// <reference no-default-lib="true"/>
+/// <reference lib="es2015" />
+/// <reference lib="dom" />
+
+/**
+ *
+ * @param {boolean} noticeFailed
+ */
 function copyToClipboard(noticeFailed) {
-  const token = htmx.find('#token').value;
+  const token = /** @type {HTMLInputElement} */ (
+    document.querySelector('#token')
+  ).value;
   setTimeout(
     async () =>
       await navigator.clipboard.writeText(token).then(
